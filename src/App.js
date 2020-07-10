1a4471a45763e7f91cc8bbb5b5ca26772b73f55b
Ram Krishnan <ramkrishnan@live.com>
2020-07-10 23:31:33 +0530

    Add toggle function

--- src/App.js
+++ src/App.js
@@ -18,15 +18,28 @@ class App extends Component {
       {
         id: 3,
         title: 'Buy some groceries',
-        completed: true
+        completed: false
       }
     ]
+  }
+
+  // Toggle a task
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+
+        return todo;
+      })
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
