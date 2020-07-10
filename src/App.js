1dc1f9a90320b1cb81c24cb698a107aa6fbefd10
Ram Krishnan <ramkrishnan@live.com>
2020-07-10 23:04:18 +0530

    Add component TodoItem

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
