dc5548a07bfc804a7207b333e1a4fbadc2d41582
Ram Krishnan <ramkrishnan@live.com>
2020-07-10 23:43:35 +0530

    Add delete task function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -22,6 +22,7 @@ export class TodoItem extends Component {
                     <input type='checkbox' onChange={this.props.markComplete.bind
                         (this, id)} /> {' '}
                     {title}
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>X</button>
                 </p>
             </div>
         )
@@ -31,6 +32,16 @@ export class TodoItem extends Component {
 // PropTypes
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 8px',
+    borderRadius: '50%',
+    curson: 'pointer',
+    float: 'right'
 }
 
 export default TodoItem
